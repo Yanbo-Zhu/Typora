900f0ae1a626ed28c5304e955020cbaa2e9b3b6b
yzh <yzh@ivu.de>
2021-04-08 11:27:25 +0200

    Typora update

---
+++ Git_Pull_Push_Integration.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#!/bin/bash
+
+printf '%s\n' "Git pull, add, commit and push"
+read -p "Do you want to update your Typora Dokument [y/n]: " trigger
+
+#echo "Input is " $tri
+
+function Git_update(){
+		echo "Git_update starts now"
+		#git pull --rebase origin master: origin/master
+		git pull --rebase origin master
+		git add -A 
+		git commit -m "Typora update"
+		git push origin master
+		git gc
+		echo "Git_update finished now"
+}
+
+if [ $trigger = y ]
+	then
+		echo "The process start now"
+		echo "The current Working directory is $PWD"
+		
+		Git_update
+		
+		echo "The process finished"
+		
+elif [ $trigger = n ]
+	then
+		echo "Input is n, this process breaks down"
+
+else
+	echo "Input is weather y nor n, this process breaks down"
+
+fi
+
+
